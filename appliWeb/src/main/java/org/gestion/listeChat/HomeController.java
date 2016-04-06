4b14adff6eba289a1ef09eb7d9b607c19f83c716
unknown <hafaud639@gmail.com>
2016-04-06 08:15:40 +0200

    mise a jour 2

--- appliWeb/src/main/java/org/gestion/listeChat/HomeController.java
+++ appliWeb/src/main/java/org/gestion/listeChat/HomeController.java
@@ -1,39 +1,76 @@
 package org.gestion.listeChat;
 
-import java.text.DateFormat;
-import java.util.Date;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Locale;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.stereotype.Controller;
+import org.gestion.listeChat.hibernate.HibernateUtil;
+import org.hibernate.Criteria;
+import org.hibernate.Session;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+
+import bean.Chat;
 
 /**
  * Handles requests for the application home page.
  */
-@Controller
+@RestController
+@RequestMapping(value = "/rest")
 public class HomeController {
-	
-	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
-	
 	/**
 	 * Simply selects the home view to render by returning its name.
 	 */
-	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public String home(Locale locale, Model model) {
-		logger.info("Welcome home! The client locale is {}.", locale);
+	@RequestMapping(value = "/chats", method = RequestMethod.GET)
+	public List<Chat> home(Locale locale, Model model) {
+//		logger.info("Welcome home! The client locale is {}.", locale);
+//		
+//		Date date = new Date();
+//		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
+//		
+//		String formattedDate = dateFormat.format(date);
+//		
+//		model.addAttribute("serverTime", formattedDate );
 		
-		Date date = new Date();
-		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
-		
-		String formattedDate = dateFormat.format(date);
-		
-		model.addAttribute("serverTime", formattedDate );
-		
-		return "home";
+		Chat chat = new Chat() ;
+		chat.setIdChat(1);
+		chat.setDescription("sqdsdqsdsq");
+		Session session = HibernateUtil.getSessionFactory().openSession() ;
+		session.beginTransaction() ;
+		Criteria c = session.createCriteria(Chat.class) ;
+		@SuppressWarnings("unchecked")
+		List<Chat> listeChats = c.list() ;
+		session.getTransaction().commit();
+		return listeChats;
 	}
 	
+	@RequestMapping(value = "/chats/delete/{idChat}", method = RequestMethod.POST,headers="Accept=application/json")
+	public void supprimerChat( @PathVariable int idChat){
+		Session session = HibernateUtil.getSessionFactory().getCurrentSession();
+		session.beginTransaction();
+		Chat c = (Chat) session.load(Chat.class, idChat);
+		session.delete(c);
+		session.getTransaction().commit();
+	}
+	@RequestMapping(value = "/chats/update/{idChat}", method = RequestMethod.POST,headers="Accept=application/json")
+	public void modifierChat( @PathVariable Chat chat){
+		Session session = HibernateUtil.getSessionFactory().getCurrentSession();
+		session.beginTransaction();
+		Chat c = (Chat) session.load(Chat.class, chat.getIdChat());
+		c.setDescription(chat.getDescription());
+		c.setImage(chat.getImage());
+		c.setMomento(chat.getMomento());
+		c.setNiveau(c.getNiveau());
+		c.setNomFrancais(chat.getNomFrancais());
+		c.setNomJaponnais(chat.getNomJaponnais());
+		session.save(c);
+		session.getTransaction().commit();
+	}
+	
+	
+
 }
--- appliWeb/src/main/webapp/js/controller/AdminController.js
+++ appliWeb/src/main/webapp/js/controller/AdminController.js
@@ -1,10 +1,36 @@
-app.controller('AdminCtrlr', ['$scope', function ($scope) {
-        $scope.supprimer = function(){
+var app=angular.module('single-page-app',['ngRoute']);
+app.controller('AdminCtrlr', ['$scope', '$http', function ($scope,$http) {
+	console.log('test') ;
+	$scope.listeChats = [] ;
+	
+	$http.get("http://localhost:8080/appliWeb/rest/chats").then(function(data) {
+		console.log(data) ;
+		$scope.listeChats = data.data ;
+	});
+	
+    $scope.supprimer = function(id){
+    	console.log(id) ;
+    	$http.post("http://localhost:8080/appliWeb/rest/chats/delete/" + id).then(function(data) {
+    		 console.log('chat supprimé') ;
+    		 $http.get("http://localhost:8080/appliWeb/rest/chats").then(function(data) {
+    				console.log(data) ;
+    				$scope.listeChats = data.data ;
+    			});
+    	}) ;
+    	
+    };
+    
+    
+    $scope.modifier = function(chat){
+    	console.log(id) ;
+    	$http.post("http://localhost:8080/appliWeb/rest/chats/update/" + chat).then(function(data) {
+    		 console.log('chat modifié') ;
+    		 $http.get("http://localhost:8080/appliWeb/rest/chats").then(function(data) {
+    				console.log(data) ;
+    				$scope.listeChats = data.data ;
+    			});
+    	}) ;
+    	
+    };
         	
-        
-        	
-        	
-        	
-        }
-        	
-}])
+}]);
